bc46740b59b2aa55cac98758739490f304a2ef70
sabiahdk <sabiahmuhd98@gmail.com>
2025-05-08 16:02:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// let valueone=parseInt(prompt("enter your first number"));
+// let valueTwo=parseInt(prompt ("enter your second name"));
+// let ans=valueone+valueTwo;
+// alert(34);
+// console.log{34}
+
+
+
+// let number = parseInt(prompt("enter a number"))
+//  if (number % 2 === 0){
+//     alert (` ${number} this is an even number`)
+//  }
+//  else{
+//    alert(`${number}this is an odd number`)
+//  }
+
+
+//   const name=John
+//  const mystory="John is a good boy,john fetches water and john does not attend parties,john teacher praises him and john does smoke";
+//  prompt(mystory);
+
+
+// const storepromt=prompt("Enter your name");
+// console.log(storepromt);
+
+
+// cons age=parseInt(prompt("Enter your age"));
+// console.log(45)
+
+
+// const userNumber=parseInt(prompt("Enter a number"));
+// if (userNumber > 0) {
+//    alert(`${usernumber}is a positive number`);
+// }else if (userNumber ===0){
+//    alert(`${userNumber}is invalid`);
+// } else if( isNaN(userNumber)) {
+//    alert(`This is not an number`)
+// } else {
+//    alert(`${}`)
+// }
+
+
+// const number=parseInt(prompt("Enter a number"));
+// let userNumber=parseInt(prompt ("enter a number"))
+// if (userNumber > number ) {
+//   alert(`${userNumber} is greater ${number}`)
+// } else if ( userNumber < number){
+//    alert (`${userNumber}is a lesser ${number}`)
+// }
+
+// const grade=prompt("Enter your grade")
+// if (grade  <= 39){
+//    alert("f, this is poor")
+// }else if( grade <= 49) {
+//    alert("D, This is fair")
+// }else if (grade <= 59){
+//    alert("c, this is good")
+// }else if( grade <= 69) {
+//    alert("B,this is very good")
+// }else if ( grade >= 70){
+//    alert("A, this is excellent")
+// } else if( grade > 100){
+//    alert("oop,invalid")
+// }
